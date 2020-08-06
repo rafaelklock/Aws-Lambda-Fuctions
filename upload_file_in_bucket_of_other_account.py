6c3eb5ddc3dc218afad5a0179340f47021becc8a
rafaelklock <42119951+rafaelklock@users.noreply.github.com>
2020-08-05 22:28:48 -0300

    Update upload_file_in_bucket_of_other_account.py

--- upload_file_in_bucket_of_other_account.py
+++ upload_file_in_bucket_of_other_account.py
@@ -3,6 +3,12 @@ import boto3
 from botocore.exceptions import ClientError
 
 """
+Permit that your lambda upload a file into a bucket's out of your account. 
+--
+Permite que sua lambda possa fazer o upload de um determinado arquivo para um Bucket de uma conta terceira(eles devem autorizar vc fazer isso).
+Alem disso, voce pode permitir que a conta hospedeira tenha permissao de leitura, etc nos arquivos carregados.
+
+kklockk@gmail.com
 
 """
 
@@ -34,27 +40,26 @@ def lambda_handler(event, context):
     
     # lista objetos
     s3 = boto3.resource('s3')
-    my_bucket = s3.Bucket('datateam.automations.dev.extracao')
+    my_bucket = s3.Bucket('other_account_bucket_name')
     for my_bucket_object in my_bucket.objects.all():
         print(my_bucket_object)
     
 
     # faz upload
     s3 = boto3.client('s3')
-    bucket_name = "datateam.automations.dev.extracao"
-    file_name = "uploaded.txt"
+    bucket_name = "other_account_bucket_name"
+    file_name = "uploaded_file.txt"
     
     with open(file_name, "rb") as f:
         s3.upload_fileobj(f, bucket_name, file_name)
         
         
-        # Libere o acesso para a conta hospedeira caso vc permita ela ler o arquivo que ela mesma hospeda:
-        
+        # Libera o acesso para a conta hospedeira, caso deseje permitir o acesso dela.
         # IdCanonico da minha conta:
-        my_cannonical_id = "3f1432a3c779b8c32dd9c30f24b2b9a5ece39741c91129d81f469b8c5679e03f"
+        my_cannonical_id = "3f1432a3c779b8c32dd88883ccc33344444433333bbbbb888f469b8c5679e03f"
         
         # IdCanonico da conta externo onde estou hospendando meu arquivo:
-        targer_cannonical_id = "f93d2cda907f441f575096cec1a577a72d101423b5592102bf4b72a8339afe96"
+        targer_cannonical_id = "f93d2cda907f4499eeee99ssbbb3333333399999999ccxx9999992a8339afe96"
         
         response = s3.put_object_acl(
             Key=file_name,
